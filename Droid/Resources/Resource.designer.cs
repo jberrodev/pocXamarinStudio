742c1c7bb186b1e162fb4be7d39f0ab22738544f
Cristal <Sogeti2013>
2016-06-16 17:08:41 +0200

    Design#1 FiltreView

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -1621,148 +1621,172 @@ namespace PocAlim.Droid
 			public const int checkbox_alimentation_generale = 2130837581;
 			
 			// aapt resource value: 0x7f02004e
-			public const int checkbox_charcuterie = 2130837582;
+			public const int checkbox_boucherie = 2130837582;
 			
 			// aapt resource value: 0x7f02004f
-			public const int checkbox_restauration_collective = 2130837583;
+			public const int checkbox_boulangerie_patisserie = 2130837583;
 			
 			// aapt resource value: 0x7f020050
-			public const int checkbox_supermarches = 2130837584;
+			public const int checkbox_charcuterie = 2130837584;
 			
 			// aapt resource value: 0x7f020051
-			public const int common_full_open_on_phone = 2130837585;
+			public const int checkbox_chocolatier = 2130837585;
 			
 			// aapt resource value: 0x7f020052
-			public const int common_google_signin_btn_icon_dark = 2130837586;
+			public const int checkbox_fromagerie = 2130837586;
 			
 			// aapt resource value: 0x7f020053
-			public const int common_google_signin_btn_icon_dark_disabled = 2130837587;
+			public const int checkbox_glacier = 2130837587;
 			
 			// aapt resource value: 0x7f020054
-			public const int common_google_signin_btn_icon_dark_focused = 2130837588;
+			public const int checkbox_poissonnerie = 2130837588;
 			
 			// aapt resource value: 0x7f020055
-			public const int common_google_signin_btn_icon_dark_normal = 2130837589;
+			public const int checkbox_restaurant = 2130837589;
 			
 			// aapt resource value: 0x7f020056
-			public const int common_google_signin_btn_icon_dark_pressed = 2130837590;
+			public const int checkbox_restauration_collective = 2130837590;
 			
 			// aapt resource value: 0x7f020057
-			public const int common_google_signin_btn_icon_light = 2130837591;
+			public const int checkbox_supermarches = 2130837591;
 			
 			// aapt resource value: 0x7f020058
-			public const int common_google_signin_btn_icon_light_disabled = 2130837592;
+			public const int checkbox_traiteur = 2130837592;
 			
 			// aapt resource value: 0x7f020059
-			public const int common_google_signin_btn_icon_light_focused = 2130837593;
+			public const int common_full_open_on_phone = 2130837593;
 			
 			// aapt resource value: 0x7f02005a
-			public const int common_google_signin_btn_icon_light_normal = 2130837594;
+			public const int common_google_signin_btn_icon_dark = 2130837594;
 			
 			// aapt resource value: 0x7f02005b
-			public const int common_google_signin_btn_icon_light_pressed = 2130837595;
+			public const int common_google_signin_btn_icon_dark_disabled = 2130837595;
 			
 			// aapt resource value: 0x7f02005c
-			public const int common_google_signin_btn_text_dark = 2130837596;
+			public const int common_google_signin_btn_icon_dark_focused = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int common_google_signin_btn_text_dark_disabled = 2130837597;
+			public const int common_google_signin_btn_icon_dark_normal = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int common_google_signin_btn_text_dark_focused = 2130837598;
+			public const int common_google_signin_btn_icon_dark_pressed = 2130837598;
 			
 			// aapt resource value: 0x7f02005f
-			public const int common_google_signin_btn_text_dark_normal = 2130837599;
+			public const int common_google_signin_btn_icon_light = 2130837599;
 			
 			// aapt resource value: 0x7f020060
-			public const int common_google_signin_btn_text_dark_pressed = 2130837600;
+			public const int common_google_signin_btn_icon_light_disabled = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int common_google_signin_btn_text_light = 2130837601;
+			public const int common_google_signin_btn_icon_light_focused = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int common_google_signin_btn_text_light_disabled = 2130837602;
+			public const int common_google_signin_btn_icon_light_normal = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int common_google_signin_btn_text_light_focused = 2130837603;
+			public const int common_google_signin_btn_icon_light_pressed = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int common_google_signin_btn_text_light_normal = 2130837604;
+			public const int common_google_signin_btn_text_dark = 2130837604;
 			
 			// aapt resource value: 0x7f020065
-			public const int common_google_signin_btn_text_light_pressed = 2130837605;
+			public const int common_google_signin_btn_text_dark_disabled = 2130837605;
 			
 			// aapt resource value: 0x7f020066
-			public const int common_ic_googleplayservices = 2130837606;
+			public const int common_google_signin_btn_text_dark_focused = 2130837606;
 			
 			// aapt resource value: 0x7f020067
-			public const int common_plus_signin_btn_icon_dark = 2130837607;
+			public const int common_google_signin_btn_text_dark_normal = 2130837607;
 			
 			// aapt resource value: 0x7f020068
-			public const int common_plus_signin_btn_icon_dark_disabled = 2130837608;
+			public const int common_google_signin_btn_text_dark_pressed = 2130837608;
 			
 			// aapt resource value: 0x7f020069
-			public const int common_plus_signin_btn_icon_dark_focused = 2130837609;
+			public const int common_google_signin_btn_text_light = 2130837609;
 			
 			// aapt resource value: 0x7f02006a
-			public const int common_plus_signin_btn_icon_dark_normal = 2130837610;
+			public const int common_google_signin_btn_text_light_disabled = 2130837610;
 			
 			// aapt resource value: 0x7f02006b
-			public const int common_plus_signin_btn_icon_dark_pressed = 2130837611;
+			public const int common_google_signin_btn_text_light_focused = 2130837611;
 			
 			// aapt resource value: 0x7f02006c
-			public const int common_plus_signin_btn_icon_light = 2130837612;
+			public const int common_google_signin_btn_text_light_normal = 2130837612;
 			
 			// aapt resource value: 0x7f02006d
-			public const int common_plus_signin_btn_icon_light_disabled = 2130837613;
+			public const int common_google_signin_btn_text_light_pressed = 2130837613;
 			
 			// aapt resource value: 0x7f02006e
-			public const int common_plus_signin_btn_icon_light_focused = 2130837614;
+			public const int common_ic_googleplayservices = 2130837614;
 			
 			// aapt resource value: 0x7f02006f
-			public const int common_plus_signin_btn_icon_light_normal = 2130837615;
+			public const int common_plus_signin_btn_icon_dark = 2130837615;
 			
 			// aapt resource value: 0x7f020070
-			public const int common_plus_signin_btn_icon_light_pressed = 2130837616;
+			public const int common_plus_signin_btn_icon_dark_disabled = 2130837616;
 			
 			// aapt resource value: 0x7f020071
-			public const int common_plus_signin_btn_text_dark = 2130837617;
+			public const int common_plus_signin_btn_icon_dark_focused = 2130837617;
 			
 			// aapt resource value: 0x7f020072
-			public const int common_plus_signin_btn_text_dark_disabled = 2130837618;
+			public const int common_plus_signin_btn_icon_dark_normal = 2130837618;
 			
 			// aapt resource value: 0x7f020073
-			public const int common_plus_signin_btn_text_dark_focused = 2130837619;
+			public const int common_plus_signin_btn_icon_dark_pressed = 2130837619;
 			
 			// aapt resource value: 0x7f020074
-			public const int common_plus_signin_btn_text_dark_normal = 2130837620;
+			public const int common_plus_signin_btn_icon_light = 2130837620;
 			
 			// aapt resource value: 0x7f020075
-			public const int common_plus_signin_btn_text_dark_pressed = 2130837621;
+			public const int common_plus_signin_btn_icon_light_disabled = 2130837621;
 			
 			// aapt resource value: 0x7f020076
-			public const int common_plus_signin_btn_text_light = 2130837622;
+			public const int common_plus_signin_btn_icon_light_focused = 2130837622;
 			
 			// aapt resource value: 0x7f020077
-			public const int common_plus_signin_btn_text_light_disabled = 2130837623;
+			public const int common_plus_signin_btn_icon_light_normal = 2130837623;
 			
 			// aapt resource value: 0x7f020078
-			public const int common_plus_signin_btn_text_light_focused = 2130837624;
+			public const int common_plus_signin_btn_icon_light_pressed = 2130837624;
 			
 			// aapt resource value: 0x7f020079
-			public const int common_plus_signin_btn_text_light_normal = 2130837625;
+			public const int common_plus_signin_btn_text_dark = 2130837625;
 			
 			// aapt resource value: 0x7f02007a
-			public const int common_plus_signin_btn_text_light_pressed = 2130837626;
-			
-			// aapt resource value: 0x7f02007d
-			public const int notification_template_icon_bg = 2130837629;
+			public const int common_plus_signin_btn_text_dark_disabled = 2130837626;
 			
 			// aapt resource value: 0x7f02007b
-			public const int splash_logo = 2130837627;
+			public const int common_plus_signin_btn_text_dark_focused = 2130837627;
 			
 			// aapt resource value: 0x7f02007c
-			public const int splash_screen = 2130837628;
+			public const int common_plus_signin_btn_text_dark_normal = 2130837628;
+			
+			// aapt resource value: 0x7f02007d
+			public const int common_plus_signin_btn_text_dark_pressed = 2130837629;
+			
+			// aapt resource value: 0x7f02007e
+			public const int common_plus_signin_btn_text_light = 2130837630;
+			
+			// aapt resource value: 0x7f02007f
+			public const int common_plus_signin_btn_text_light_disabled = 2130837631;
+			
+			// aapt resource value: 0x7f020080
+			public const int common_plus_signin_btn_text_light_focused = 2130837632;
+			
+			// aapt resource value: 0x7f020081
+			public const int common_plus_signin_btn_text_light_normal = 2130837633;
+			
+			// aapt resource value: 0x7f020082
+			public const int common_plus_signin_btn_text_light_pressed = 2130837634;
+			
+			// aapt resource value: 0x7f020085
+			public const int notification_template_icon_bg = 2130837637;
+			
+			// aapt resource value: 0x7f020083
+			public const int splash_logo = 2130837635;
+			
+			// aapt resource value: 0x7f020084
+			public const int splash_screen = 2130837636;
 			
 			static Drawable()
 			{
@@ -1777,14 +1801,14 @@ namespace PocAlim.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f0c006e
-			public const int FilterOkBtn = 2131492974;
+			// aapt resource value: 0x7f0c0076
+			public const int FilterOkBtn = 2131492982;
 			
-			// aapt resource value: 0x7f0c0073
-			public const int FiltreBtn = 2131492979;
+			// aapt resource value: 0x7f0c007b
+			public const int FiltreBtn = 2131492987;
 			
-			// aapt resource value: 0x7f0c006f
-			public const int FiltreCloseBtn = 2131492975;
+			// aapt resource value: 0x7f0c0077
+			public const int FiltreCloseBtn = 2131492983;
 			
 			// aapt resource value: 0x7f0c0009
 			public const int MvvmCrossTagId = 2131492873;
@@ -1882,8 +1906,8 @@ namespace PocAlim.Droid
 			// aapt resource value: 0x7f0c0043
 			public const int customPanel = 2131492931;
 			
-			// aapt resource value: 0x7f0c0070
-			public const int custom_marker_popup_title = 2131492976;
+			// aapt resource value: 0x7f0c0078
+			public const int custom_marker_popup_title = 2131492984;
 			
 			// aapt resource value: 0x7f0c002a
 			public const int dark = 2131492906;
@@ -1915,6 +1939,15 @@ namespace PocAlim.Droid
 			// aapt resource value: 0x7f0c006a
 			public const int filter1 = 2131492970;
 			
+			// aapt resource value: 0x7f0c0073
+			public const int filter10 = 2131492979;
+			
+			// aapt resource value: 0x7f0c0074
+			public const int filter11 = 2131492980;
+			
+			// aapt resource value: 0x7f0c0075
+			public const int filter12 = 2131492981;
+			
 			// aapt resource value: 0x7f0c006b
 			public const int filter2 = 2131492971;
 			
@@ -1923,6 +1956,21 @@ namespace PocAlim.Droid
 			
 			// aapt resource value: 0x7f0c006d
 			public const int filter4 = 2131492973;
+			
+			// aapt resource value: 0x7f0c006e
+			public const int filter5 = 2131492974;
+			
+			// aapt resource value: 0x7f0c006f
+			public const int filter6 = 2131492975;
+			
+			// aapt resource value: 0x7f0c0070
+			public const int filter7 = 2131492976;
+			
+			// aapt resource value: 0x7f0c0071
+			public const int filter8 = 2131492977;
+			
+			// aapt resource value: 0x7f0c0072
+			public const int filter9 = 2131492978;
 			
 			// aapt resource value: 0x7f0c0004
 			public const int home = 2131492868;
@@ -1963,8 +2011,8 @@ namespace PocAlim.Droid
 			// aapt resource value: 0x7f0c0035
 			public const int list_item = 2131492917;
 			
-			// aapt resource value: 0x7f0c0071
-			public const int map = 2131492977;
+			// aapt resource value: 0x7f0c0079
+			public const int map = 2131492985;
 			
 			// aapt resource value: 0x7f0c0060
 			public const int media_actions = 2131492960;
@@ -1975,8 +2023,8 @@ namespace PocAlim.Droid
 			// aapt resource value: 0x7f0c0016
 			public const int multiply = 2131492886;
 			
-			// aapt resource value: 0x7f0c0072
-			public const int myLayout = 2131492978;
+			// aapt resource value: 0x7f0c007a
+			public const int myLayout = 2131492986;
 			
 			// aapt resource value: 0x7f0c0021
 			public const int never = 2131492897;
@@ -2327,25 +2375,70 @@ namespace PocAlim.Droid
 			public const int pin_alimentation_generale_off = 2130903047;
 			
 			// aapt resource value: 0x7f030008
-			public const int pin_boucherie_off = 2130903048;
+			public const int pin_boucherie = 2130903048;
 			
 			// aapt resource value: 0x7f030009
-			public const int pin_charcuterie = 2130903049;
+			public const int pin_boucherie_off = 2130903049;
 			
 			// aapt resource value: 0x7f03000a
-			public const int pin_charcuterie_off = 2130903050;
+			public const int pin_boulangerie_patisserie = 2130903050;
 			
 			// aapt resource value: 0x7f03000b
-			public const int pin_restauration_collective = 2130903051;
+			public const int pin_boulangerie_patisserie_off = 2130903051;
 			
 			// aapt resource value: 0x7f03000c
-			public const int pin_restauration_collective_off = 2130903052;
+			public const int pin_charcuterie = 2130903052;
 			
 			// aapt resource value: 0x7f03000d
-			public const int pin_supermarches_hypermarches = 2130903053;
+			public const int pin_charcuterie_off = 2130903053;
 			
 			// aapt resource value: 0x7f03000e
-			public const int pin_supermarches_hypermarches_off = 2130903054;
+			public const int pin_chocolatier = 2130903054;
+			
+			// aapt resource value: 0x7f03000f
+			public const int pin_chocolatier_off = 2130903055;
+			
+			// aapt resource value: 0x7f030010
+			public const int pin_fromagerie = 2130903056;
+			
+			// aapt resource value: 0x7f030011
+			public const int pin_fromagerie_off = 2130903057;
+			
+			// aapt resource value: 0x7f030012
+			public const int pin_glacier = 2130903058;
+			
+			// aapt resource value: 0x7f030013
+			public const int pin_glacier_off = 2130903059;
+			
+			// aapt resource value: 0x7f030014
+			public const int pin_poissonnerie = 2130903060;
+			
+			// aapt resource value: 0x7f030015
+			public const int pin_poissonnerie_off = 2130903061;
+			
+			// aapt resource value: 0x7f030016
+			public const int pin_restaurant = 2130903062;
+			
+			// aapt resource value: 0x7f030017
+			public const int pin_restaurant_off = 2130903063;
+			
+			// aapt resource value: 0x7f030018
+			public const int pin_restauration_collective = 2130903064;
+			
+			// aapt resource value: 0x7f030019
+			public const int pin_restauration_collective_off = 2130903065;
+			
+			// aapt resource value: 0x7f03001a
+			public const int pin_supermarches_hypermarches = 2130903066;
+			
+			// aapt resource value: 0x7f03001b
+			public const int pin_supermarches_hypermarches_off = 2130903067;
+			
+			// aapt resource value: 0x7f03001c
+			public const int pin_traiteur = 2130903068;
+			
+			// aapt resource value: 0x7f03001d
+			public const int pin_traiteur_off = 2130903069;
 			
 			static Mipmap()
 			{
@@ -2426,11 +2519,11 @@ namespace PocAlim.Droid
 			// aapt resource value: 0x7f060030
 			public const int auth_google_play_services_client_google_display_name = 2131099696;
 			
-			// aapt resource value: 0x7f06003b
-			public const int button_cancel = 2131099707;
+			// aapt resource value: 0x7f060043
+			public const int button_cancel = 2131099715;
 			
-			// aapt resource value: 0x7f06003a
-			public const int button_ok = 2131099706;
+			// aapt resource value: 0x7f060042
+			public const int button_ok = 2131099714;
 			
 			// aapt resource value: 0x7f060029
 			public const int common_google_play_services_api_unavailable_text = 2131099689;
@@ -2522,6 +2615,15 @@ namespace PocAlim.Droid
 			// aapt resource value: 0x7f060036
 			public const int filter_1 = 2131099702;
 			
+			// aapt resource value: 0x7f06003f
+			public const int filter_10 = 2131099711;
+			
+			// aapt resource value: 0x7f060040
+			public const int filter_11 = 2131099712;
+			
+			// aapt resource value: 0x7f060041
+			public const int filter_12 = 2131099713;
+			
 			// aapt resource value: 0x7f060037
 			public const int filter_2 = 2131099703;
 			
@@ -2530,6 +2632,21 @@ namespace PocAlim.Droid
 			
 			// aapt resource value: 0x7f060039
 			public const int filter_4 = 2131099705;
+			
+			// aapt resource value: 0x7f06003a
+			public const int filter_5 = 2131099706;
+			
+			// aapt resource value: 0x7f06003b
+			public const int filter_6 = 2131099707;
+			
+			// aapt resource value: 0x7f06003c
+			public const int filter_7 = 2131099708;
+			
+			// aapt resource value: 0x7f06003d
+			public const int filter_8 = 2131099709;
+			
+			// aapt resource value: 0x7f06003e
+			public const int filter_9 = 2131099710;
 			
 			// aapt resource value: 0x7f060035
 			public const int filter_button_text = 2131099701;
